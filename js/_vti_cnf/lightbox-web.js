075b26b632681e80c05025c3dab18ef68aa78457
rytirnik <rytirnik@gmail.com>
2014-09-27 18:44:43 +0200

    first commit

---
+++ js/_vti_cnf/lightbox-web.js
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|31 May 2010 08:06:58 -0000
+vti_extenderversion:SR|6.0.2.8161
+vti_cacheddtm:TX|31 May 2010 08:06:58 -0000
+vti_filesize:IR|18389
+vti_backlinkinfo:VX|
